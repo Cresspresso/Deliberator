42c3fca71b4bce6fa94c0bdd8789af6723e151e0
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-04-27 11:34:31 +1200

    Scene Merger

---
+++ Assets/Scripts/SceneMergeObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SceneMergeObject : MonoBehaviour
+{
+	private void Start()
+	{
+		var merger = FindObjectOfType<SceneMerger>();
+		if (merger && merger.objects.Contains(this) == false)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+}
---
+++ Assets/Scripts/SceneMerger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMerger : MonoBehaviour
+{
+	public string[] sceneNames = new string[0];
+	public HashSet<SceneMergeObject> objects { get; private set; }
+
+	private void Awake()
+	{
+		if (FindObjectOfType<SceneMerger>() != this)
+		{
+			Debug.LogError("Two SceneMergers in the same Scene", this);
+			return;
+		}
+
+		objects = new HashSet<SceneMergeObject>(FindObjectsOfType<SceneMergeObject>());
+	}
+
+	private void Start()
+	{
+		foreach (var name in sceneNames)
+		{
+			try
+			{
+				SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+			}
+			catch (Exception e)
+			{
+				Debug.LogException(e, this);
+			}
+		}
+	}
+}
