35f340594f721681c036440d81dcf5894ad6309f
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-04-27 11:56:37 +1200

    common scene objects

--- Assets/Scripts/SceneMergeObject.cs
+++ Assets/Scripts/SceneMergeObject.cs
@@ -1,17 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using UnityEngine;
 
+/// <author>Elijah Shadbolt</author>
 public class SceneMergeObject : MonoBehaviour
 {
-	private void Start()
+	private static Dictionary<Hash128, SceneMergeObject> objects = new Dictionary<Hash128, SceneMergeObject>();
+
+	[SerializeField]
+	private string m_uniqueName;
+
+	private Hash128 uniqueNameHash;
+	private bool isTheOne = false;
+
+	private void Awake()
 	{
-		var merger = FindObjectOfType<SceneMerger>();
-		if (merger && merger.objects.Contains(this) == false)
+		if (string.IsNullOrWhiteSpace(m_uniqueName))
 		{
+			Debug.LogError("Unique Name is missing", this);
 			Destroy(gameObject);
 			return;
+		}
+
+		uniqueNameHash = Hash128.Compute(m_uniqueName);
+		if (objects.TryGetValue(uniqueNameHash, out SceneMergeObject obj))
+		{
+			if (obj != this)
+			{
+				Destroy(gameObject);
+			}
+		}
+		else
+		{
+			isTheOne = true;
+			objects.Add(uniqueNameHash, this);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (isTheOne)
+		{
+			objects.Remove(uniqueNameHash);
 		}
 	}
 }
--- Assets/Scripts/SceneMerger.cs
+++ Assets/Scripts/SceneMerger.cs
@@ -1,24 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <author>Elijah Shadbolt</author>
 public class SceneMerger : MonoBehaviour
 {
 	public string[] sceneNames = new string[0];
-	public HashSet<SceneMergeObject> objects { get; private set; }
-
-	private void Awake()
-	{
-		if (FindObjectOfType<SceneMerger>() != this)
-		{
-			Debug.LogError("Two SceneMergers in the same Scene", this);
-			return;
-		}
-
-		objects = new HashSet<SceneMergeObject>(FindObjectsOfType<SceneMergeObject>());
-	}
 
 	private void Start()
 	{
