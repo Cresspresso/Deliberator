49cf356ec596155eebad82b4e1a139f38eb7161f
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-05-11 15:38:53 +1200

    rewind cutscene

---
+++ Assets/Scripts/Rewind Cutscene/RewindCapture.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public struct RewindMoment
+{
+	public float duration { get; private set; }
+	public Texture2D texture { get; private set; }
+
+	public RewindMoment(float duration, Texture2D texture)
+	{
+		this.duration = duration;
+		this.texture = texture;
+	}
+}
+
+/// <summary>
+/// Put this in a game scene.
+/// Call <see cref="PresentRewindCutscene"/> to load the rewind cutscene.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
+public class RewindCapture : MonoBehaviour
+{
+	public AnimationCurve durationBetweenFramesOverTime = new AnimationCurve(
+		new Keyframe(0, 1.0f / 60.0f),
+		new Keyframe(10 * 60, 10));
+	public Stack<RewindMoment> moments { get; private set; } = new Stack<RewindMoment>();
+	public float elapsedTime { get; private set; } = 0.0f;
+	public float momentDuration { get; private set; } = 0.0f;
+
+	private void Awake()
+	{
+		OnEnable();
+	}
+
+	private void OnEnable()
+	{
+		if (coroutineCaptureScreen == null)
+		{
+			coroutineCaptureScreen = StartCoroutine(CoroutineCaptureScreen());
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (coroutineCaptureScreen != null)
+		{
+			StopCoroutine(coroutineCaptureScreen);
+			coroutineCaptureScreen = null;
+		}
+	}
+
+	private Coroutine coroutineCaptureScreen;
+	private IEnumerator CoroutineCaptureScreen()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(durationBetweenFramesOverTime.Evaluate(elapsedTime));
+			moments.Push(new RewindMoment(momentDuration, ScreenCapture.CaptureScreenshotAsTexture()));
+			momentDuration = 0.0f;
+		}
+	}
+
+	public void PresentRewindCutscene()
+	{
+		var data = FindObjectOfType<RewindData>();
+		if (!data)
+		{
+			var go = new GameObject("Rewind Data");
+			DontDestroyOnLoad(go);
+			data = go.AddComponent<RewindData>();
+		}
+		data.moments = this.moments;
+		data.sceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene("Rewind Cutscene");
+	}
+
+	private void Update()
+	{
+		elapsedTime += Time.deltaTime;
+		momentDuration += Time.deltaTime;
+
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			PresentRewindCutscene();
+		}
+	}
+}
---
+++ Assets/Scripts/Rewind Cutscene/RewindCutscene.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Put this in its own scene with a RawImage.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
+public class RewindCutscene : MonoBehaviour
+{
+	public float duration = 10.0f;
+	public RawImage rawImage;
+	private RewindData data;
+	private float speed;
+	private float momentDuration;
+	private float elapsed = 0.0f;
+	public bool isFinished { get; private set; } = false;
+
+	private void Awake()
+	{
+		data = FindObjectOfType<RewindData>();
+		if (!data)
+		{
+			Debug.LogError("RewindData not found", this);
+			SceneManager.LoadScene(0);
+			return;
+		}
+		if (!rawImage)
+		{
+			Debug.LogError("rawImage is null", this);
+			InvokeFinished();
+			return;
+		}
+		if (duration <= 0)
+		{
+			Debug.LogError("duration <= 0", this);
+			InvokeFinished();
+			return;
+		}
+
+		CursorController.UncheckedSetCursorHidden(true);
+		if (!data.moments.Any())
+		{
+			Debug.LogError("no moments", this);
+			InvokeFinished();
+			return;
+		}
+
+		var recordedDuration = data.moments.Sum(m => m.duration);
+		speed = recordedDuration / duration;
+
+		var moment = data.moments.Pop();
+		momentDuration = moment.duration;
+		rawImage.texture = moment.texture;
+	}
+
+	private void Update()
+	{
+		if (isFinished) { return; }
+		elapsed += Time.deltaTime * speed;
+		while (elapsed >= momentDuration)
+		{
+			elapsed -= momentDuration;
+			if (data.moments.Any())
+			{
+				var moment = data.moments.Pop();
+				momentDuration = moment.duration;
+				rawImage.texture = moment.texture;
+			}
+			else
+			{
+				InvokeFinished();
+				break;
+			}
+		}
+	}
+
+	private void OnDestroy()
+	{
+		CursorController.UncheckedSetCursorHidden(false);
+	}
+
+	private void InvokeFinished()
+	{
+		isFinished = true;
+		SceneManager.LoadScene(data.sceneBuildIndex);
+	}
+}
