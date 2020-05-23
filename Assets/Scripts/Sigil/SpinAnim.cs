3703f18ad269dd5c59d2f4973a71306b16bffd72
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-05-24 09:59:22 +1200

    Sigil

---
+++ Assets/Scripts/Sigil/SpinAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpinAnim : MonoBehaviour
+{
+	private void Start()
+	{
+		transform.DORotate(new Vector3(0, 0, 360.0f), 1.0f, RotateMode.LocalAxisAdd)
+			.SetLoops(-1)
+			.SetEase(Ease.Linear);
+	}
+}
