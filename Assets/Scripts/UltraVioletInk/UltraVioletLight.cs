c14011243cd97b0a17b2be24cace2b83d6211c07
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-05-25 17:20:10 +1200

    invisible ink

---
+++ Assets/Scripts/UltraVioletInk/UltraVioletLight.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltraVioletLight : MonoBehaviour
+{
+	[Range(0.0f, 180.0f)]
+	public float spotAngle = 30.0f;
+
+	[Range(0.0f, 100.0f)]
+	public float innerSpotAnglePercent = 0.0f;
+}
