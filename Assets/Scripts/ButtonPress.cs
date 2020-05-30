465e9c2d148a462150eff696c3a5b8af69437ef1
Jordyn <53497238+Ooogway@users.noreply.github.com>
2020-05-30 16:13:34 +1200

    Buttons

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    Animator anim;
+    public AudioSource buttonSound;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        AudioSource audio = GetComponent<AudioSource>();
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+            buttonSound.Play();
+        }
+    }
+}
