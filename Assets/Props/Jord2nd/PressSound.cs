a24ddaae86085ae9e606d5c0a6784db8ecb0aea9
Jordyn <53497238+Ooogway@users.noreply.github.com>
2020-05-31 19:19:26 +1200

    Audio syncs

---
+++ Assets/Props/Jord2nd/PressSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressSound : MonoBehaviour
+{
+    public AudioClip slam;
+
+    public AudioSource audioSource;
+    
+    void Slam()
+    {
+        audioSource.PlayOneShot(slam);
+    }
+
+}
