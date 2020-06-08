42ecb566fb94b0f9dcfce3d2f77f40264b7da446
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2020-06-08 17:11:43 +1200

    box press item spawner

---
+++ Assets/pressItemSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <author> Lorenzo Sae-Phoo Zemp </author>
+public class pressItemSpawner : MonoBehaviour
+{
+    public GameObject blueItem;
+    public GameObject redItem;
+    public GameObject greenItem;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "BoxBlue")
+        {
+            Destroy(col.gameObject);
+            Instantiate(blueItem, gameObject.transform);
+        }
+        else if (col.tag == "BoxGreen")
+        {
+            Destroy(col.gameObject);
+            Instantiate(greenItem, gameObject.transform);
+        }
+        else if (col.tag == "BoxRed")
+        {
+            Destroy(col.gameObject);
+            Instantiate(redItem, gameObject.transform);
+
+        }
+    }
+}
