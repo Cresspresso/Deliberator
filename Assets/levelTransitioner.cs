327daeb33445ccebe3f45ff893c3ab2e519e1e44
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2020-06-11 01:03:00 +1200

    Level Transition in elevator travel from hub to box scene

---
+++ Assets/levelTransitioner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/// <author> Lorenzo Sae-Phoo Zemp /// </author>
+public class levelTransitioner : MonoBehaviour
+{
+    public int levelIndex;
+
+    public Animator fadeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeAnim = GameObject.FindGameObjectWithTag("SceneFader").GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            FadeToLevel();
+        }
+    }
+
+    public void FadeToLevel ()
+    {
+        fadeAnim.SetTrigger("Fade");
+        StartCoroutine(WaitToTransition());
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    IEnumerator WaitToTransition()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
