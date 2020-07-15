d3a57cdfb2e9da0b4db80f4038df0896de6cc0d5
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2020-07-15 16:23:14 +1200

    Settings Menu in Pause Menu implemented

---
+++ Assets/BisonAlphaProduction/Scripts/V3_SettingsMenu.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class V3_SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public GameObject player;
+
+    public Dropdown resolutionDropdown;
+
+    private Resolution[] availableResolutions;
+
+    void Awake()
+    {
+        availableResolutions = Screen.resolutions;
+        SetUpResolutionSettings();
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void SetUpResolutionSettings()
+    {
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionOptions = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < availableResolutions.Length; i++)
+        {
+            string option = availableResolutions[i].width + "x" + availableResolutions[i].height;
+            resolutionOptions.Add(option);
+
+            if (availableResolutions[i].width == Screen.currentResolution.width &&
+                availableResolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int _resolutionIndex)
+    {
+        Resolution resolution = availableResolutions[_resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreen (bool _fullscreen)
+    {
+        Screen.fullScreen = _fullscreen;
+    }
+
+    public void SetQuality (int _qualityIndex) // 0 low 1 medium 2 high
+    {
+        QualitySettings.SetQualityLevel(_qualityIndex);
+    }
+
+    public void SetVolume (float _volume)
+    {
+        audioMixer.SetFloat("MasterVolume", _volume);
+    }
+
+    public void SetSensitivity (float _sensitivity)
+    {
+        player.GetComponent<V2_FirstPersonCharacterController>().mouseSensitivity = _sensitivity;
+    }
+}
