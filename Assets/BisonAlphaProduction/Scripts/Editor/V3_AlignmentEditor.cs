1f28ab0e06d5a63fc3a5c02aeb8f81f6c1c07bd4
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-07-27 17:17:41 +1200

    Alignment script

---
+++ Assets/BisonAlphaProduction/Scripts/Editor/V3_AlignmentEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(V3_Alignment))]
+public class V3_AlignmentEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Align"))
+		{
+			var script = (V3_Alignment)target;
+			var children = new List<Transform>();
+			foreach (Transform child in script.transform)
+			{
+				children.Add(child);
+			}
+			Undo.RecordObjects(children.ToArray(), "Align");
+			script.Align();
+		}
+	}
+}
---
+++ Assets/BisonAlphaProduction/Scripts/V3_Alignment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V3_Alignment : MonoBehaviour
+{
+	public Vector3 elementOffset = Vector3.forward;
+
+	private void Update()
+	{
+		Align();
+	}
+
+	public void Align()
+	{
+		var c = transform.childCount;
+		for (int i = 0; i < c; ++i)
+		{
+			Transform child = transform.GetChild(i);
+			child.localPosition = i * elementOffset;
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawRay(transform.position, transform.TransformVector(elementOffset));
+	}
+}
