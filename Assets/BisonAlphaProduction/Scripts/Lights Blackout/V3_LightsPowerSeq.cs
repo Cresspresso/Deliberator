9cceb6dd8096059b5e7b269ee98bf9d3d0fd47cb
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-07-28 11:36:12 +1200

    lights turning off one by one

---
+++ Assets/BisonAlphaProduction/Scripts/Lights Blackout/V3_LightsPowerSeq.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Turns on/off all lights in sequence.
+/// Each child Transform of this GameObject is considered a row of lights.
+/// A row of lights is a parent Transform with lights as children.
+/// </summary>
+/// <author>Elijah Shadbolt</author>
+public class V3_LightsPowerSeq : MonoBehaviour
+{
+	public float delayBetweenRows = 1.0f;
+	//public AnimationCurve delayCurve = new AnimationCurve(new Keyframe(0, 0.5f), new Keyframe(1, 0.5f));
+
+	public bool isAnimationPlaying { get; private set; } = false;
+
+	public void TurnLightsOff()
+	{
+		if (!isAnimationPlaying)
+		{
+			isAnimationPlaying = true;
+			StartCoroutine(CoEnableLights(false));
+		}
+	}
+
+	private IEnumerator CoEnableLights(bool enable)
+	{
+		for (int i = 0; i < transform.childCount; ++i)
+		{
+			foreach (Light light in transform.GetChild(i)
+				.GetComponentsInChildren<Light>())
+			{
+				light.enabled = enable;
+			}
+			//float delay = delayCurve.Evaluate((float)i / (float)(transform.childCount - 1));
+			float delay = delayBetweenRows;
+			yield return new WaitForSeconds(delay);
+		}
+		isAnimationPlaying = false;
+	}
+}
---
+++ Assets/BisonAlphaProduction/Scripts/Lights Blackout/V3_TestLights.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V3_TestLights : MonoBehaviour
+{
+	public V3_LightsPowerSeq lights;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			lights.TurnLightsOff();
+			Destroy(gameObject);
+		}
+	}
+}
