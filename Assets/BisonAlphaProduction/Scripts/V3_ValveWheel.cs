fbde5c0f689df1a1ccdf290ce286414b74eec39f
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2020-07-28 15:02:52 +1200

    Valve

--- Assets/BisonAlphaProduction/Scripts/V3_ValveWheel.cs
+++ Assets/BisonAlphaProduction/Scripts/V3_ValveWheel.cs
@@ -10,13 +10,17 @@ using UnityEngine;
 [RequireComponent(typeof(V2_Handle))]
 public class V3_ValveWheel : MonoBehaviour
 {
+    delegate void DidChangeState(bool _bool);
+    DidChangeState didChangeState;
+
     public float interactableDistance = 2.5f;
+    public GameObject targetObj; // change datatype depending on what component i want to access
 
     private V2_Handle hoverHandle;
     private V3_ProximityCalculator proximityCalculator;
     private Animator animator;
 
-    private bool activatable = false;
+    [SerializeField] private bool activatable = false;
     private bool activated = false;
 
     // Start is called before the first frame update
@@ -25,6 +29,8 @@ public class V3_ValveWheel : MonoBehaviour
         proximityCalculator = gameObject.GetComponent<V3_ProximityCalculator>();
         animator = gameObject.GetComponent<Animator>();
         hoverHandle = gameObject.GetComponent<V2_Handle>();
+
+        //didChangeState = DoorUnlock;
     }
 
     // When clicked on
@@ -39,8 +45,18 @@ public class V3_ValveWheel : MonoBehaviour
             activatable = false;
 
             hoverHandle.hoverInfo = new V2_HandleHoverInfo("Opened", null);
+
+            //didChangeState(activated);
         }
     }
+
+    #region Delegates
+    void DoorUnlock(bool _bool)
+    {
+        //targetObj.GetComponent<V2_SwingDoor>().isLocked = _bool;
+    }
+
+    #endregion
 
     public void setActivatable(bool _bool)
     {
