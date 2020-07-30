db3b034baf0ad8a6983d0c0d31fb4a6d9ae0c7c6
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2020-07-30 17:19:22 +1200

    Condensation Thing

---
+++ Assets/V3_Condensation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+/// <author>Lorenzo Sae-Phoo Zemp</author>
+public class V3_Condensation : MonoBehaviour
+{
+    [SerializeField] private bool activate = false;
+    [SerializeField] private Material condensationMaterial;
+    [SerializeField] private float fadeSpeed = 0.1f;
+
+    private Color colorTarget = new Color(1.0f, 1.0f, 1.0f, 1.0f); //fully visible
+    private Color colorStart;
+    private Color colorCurrent;
+    private float timer = 0.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        condensationMaterial = gameObject.GetComponent<Renderer>().material;
+        colorStart = gameObject.GetComponent<Renderer>().material.color;
+        colorCurrent = colorStart;
+    }
+
+    void Update()
+    {
+        if (activate && timer <= 1.0)
+        {
+            colorCurrent = Color.Lerp(colorStart, colorTarget, timer);
+            //Debug.Log(colorCurrent);
+            condensationMaterial.SetColor("_BaseColor", colorCurrent);
+
+            timer += Time.deltaTime * fadeSpeed;
+           // Debug.Log(condensationMaterial.color);
+        }
+    }
+
+    public void Activate()
+    {
+        activate = true;
+    }
+}
