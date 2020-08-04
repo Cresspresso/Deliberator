a9f5e0b06d4eec37e889d13061d0da00a0e0a5a4
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2020-08-05 11:40:09 +1200

    implementing decon

--- Assets/BisonVerticalSlice/V2_pressItemSpawner.cs
+++ Assets/BisonVerticalSlice/V2_pressItemSpawner.cs
@@ -21,17 +21,26 @@ public class V2_pressItemSpawner : MonoBehaviour
         if (col.tag == "BoxBlue")
         {
             Destroy(col.gameObject);
-            Instantiate(blueItem, gameObject.transform);
+            if (blueItem != null)
+            {
+                Instantiate(blueItem, gameObject.transform);
+            }
         }
         else if (col.tag == "BoxGreen")
         {
             Destroy(col.gameObject);
-            Instantiate(greenItem, gameObject.transform);
+            if (greenItem != null)
+            {
+                Instantiate(greenItem, gameObject.transform);
+            }
         }
         else if (col.tag == "BoxRed")
         {
             Destroy(col.gameObject);
-            Instantiate(redItem, gameObject.transform);
+            if (redItem != null)
+            {
+                Instantiate(redItem, gameObject.transform);
+            }
         }
     }
 }
