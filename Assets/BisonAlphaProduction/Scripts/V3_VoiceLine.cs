93851b806de45962086439381d58bffee91722b5
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-08-10 17:08:59 +1200

    Comments for VoiceLine scripts

---
+++ Assets/BisonAlphaProduction/Scripts/V3_VoiceLine.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+/// <summary>
+///		<para>Data structure for a voice line audio clip and closed captions text.</para>
+///		<para>Can be drawn in the inspector.</para>
+///		<para>See also:</para>
+///		<para><see cref="V3_VoiceLineTrigger.voiceLines"/></para>
+///		<para><see cref="V3_VoiceLineManager"/></para>
+///		<para><see cref="V3_VoiceLineAudioSource"/></para>
+/// </summary>
+/// 
+/// <changelog>
+///		<log author="Elijah Shadbolt" date="10/08/2020">
+///			<para>Added comments.</para>
+///		</log>
+/// </changelog>
+/// 
+[Serializable]
+public class VoiceLine
+{
+	/// <summary>
+	///		<para>The audio clip to play as part of this voice line, or null.</para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			<para>Added comments.</para>
+	///		</log>
+	/// </changelog>
+	/// 
+	[Tooltip(@"
+The audio clip to play as part of this voice line, or null.
+")]
+	public AudioClip clip;
+
+
+
+	/// <summary>
+	///		<para>If <see cref="clip"/> is null, this value is used in place of the clip's <see cref="AudioClip.length"/>.</para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			<para>Added comments.</para>
+	///		</log>
+	/// </changelog>
+	/// 
+	[Tooltip(@"
+If Clip is null, this value is used in place of the clip's length.
+")]
+	public float clipDurationIfNull = 0.0f;
+
+
+
+	/// <summary>
+	///		<para>
+	///			Time added after the audio clip has finished playing,
+	///			inserted before the voice line is considered finished playing.
+	///		</para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			<para>Added comments.</para>
+	///		</log>
+	/// </changelog>
+	/// 
+	[Tooltip(@"
+Time added after the audio clip has finished playing,
+inserted before the voice line is considered finished playing.
+")]
+	public float paddingDuration = 0.1f;
+
+
+
+	/// <summary>
+	///		<para>The closed caption text to go along with the audio clip.</para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			<para>Added comments.</para>
+	///		</log>
+	/// </changelog>
+	/// 
+	[Tooltip(@"
+The closed caption text to go along with the audio clip.
+")]
+	[TextArea(1, 3)]
+	public string subtitle = "<Missing Subtitle>";
+}
--- Assets/BisonAlphaProduction/Scripts/V3_VoiceLineAudioSource.cs
+++ Assets/BisonAlphaProduction/Scripts/V3_VoiceLineAudioSource.cs
@@ -2,14 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <author>Elijah Shadbolt</author>
-/// <stage>Alpha Production</stage>
+/// <summary>
+///		<para>Plays an <see cref="AudioClip"/> sent by the <see cref="V3_VoiceLineManager"/> when a new voiceline begins playing.</para>
+///		<para>Attach to a <see cref="GameObject"/> with an <see cref="AudioSource"/>.</para>
+///		<para>The <see cref="AudioSource"/> can have <see cref="AudioSource.spatialBlend"/> set to full 3D.</para>
+///		<para>See also:</para>
+///		<para><see cref="V3_VoiceLineManager"/></para>
+///		<para><see cref="V3_VoiceLineTrigger"/></para>
+/// </summary>
+/// 
+/// <changelog>
+///		<log author="Elijah Shadbolt" date="10/08/2020">
+///			Added comments.
+///		</log>
+/// </changelog>
+/// 
 [RequireComponent(typeof(AudioSource))]
 public class V3_VoiceLineAudioSource : MonoBehaviour
 {
+	/// <summary>
+	///		<para>The manager that this script has registered itself with.</para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			Added comments.
+	///		</log>
+	/// </changelog>
+	/// 
 	private V3_VoiceLineManager manager;
+
+
+
+	/// <summary>
+	///		<para>Required component.</para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			Added comments.
+	///		</log>
+	/// </changelog>
+	/// 
 	public AudioSource audioSource { get; private set; }
 
+
+
+	/// <summary>
+	///		<para>Plays an audio clip.</para>
+	///		<para>This is called by this script's manager <see cref="manager"/>.</para>
+	///		<para>See also:</para>
+	///		<para><see cref="V3_VoiceLineManager.PopAndPlayVoiceLine"/></para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			Added comments.
+	///		</log>
+	/// </changelog>
+	/// 
 	public void Play(AudioClip clip)
 	{
 		audioSource.Stop();
@@ -17,23 +68,67 @@ public class V3_VoiceLineAudioSource : MonoBehaviour
 		audioSource.Play();
 	}
 
+
+
+	/// <summary>
+	///		<para>Stops playing the audio clip (if one was playing).</para>
+	///		<para>This is called by this script's manager <see cref="manager"/>.</para>
+	///		<para>See also:</para>
+	///		<para><see cref="V3_VoiceLineManager.PopAndPlayVoiceLine"/></para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			Added comments.
+	///		</log>
+	/// </changelog>
+	/// 
 	public void Stop()
 	{
 		audioSource.Stop();
 	}
 
+
+
+	/// <summary>
+	///		<para>Unity Message Method: <a href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html"></a></para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			Added comments.
+	///		</log>
+	/// </changelog>
+	/// 
 	private void Start()
 	{
 		audioSource = GetComponent<AudioSource>();
 
+		/// This code must be in <see cref="Start"/>,
+		/// after <see cref="V3_VoiceLineManager.instance"/>
+		/// is populated in <see cref="V3_VoiceLineManager.Awake"/>.
+		/// 
 		manager = V3_VoiceLineManager.instance;
 		Debug.Assert(manager);
 		manager.Register(this);
 	}
 
+
+
+	/// <summary>
+	///		<para>Unity Message Method: <a href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnDestroy.html"></a></para>
+	/// </summary>
+	/// 
+	/// <changelog>
+	///		<log author="Elijah Shadbolt" date="10/08/2020">
+	///			Added comments.
+	///		</log>
+	/// </changelog>
+	/// 
 	private void OnDestroy()
 	{
-		if (manager) // the manager could be destroyed before this one
+		/// Life check because the manager could be destroyed before this script.
+		if (manager)
 		{
 			manager.Unregister(this);
 		}
