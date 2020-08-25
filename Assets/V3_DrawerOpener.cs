860a2a92b86015cc3747a63b23938cf73d5c48de
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2020-08-25 17:03:22 +1200

    Drawer Implementation 1 down 7 to go

---
+++ Assets/V3_DrawerOpener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script manages the interactions between the player and the drawer
+/// </summary>
+/// <author>Lorenzo Sae-Phoo Zemp</author>
+public class V3_DrawerOpener : MonoBehaviour
+{
+    private Animator animator;
+    private V2_Handle handle;
+
+    private bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        handle = gameObject.GetComponent<V2_Handle>();
+    }
+
+    public void TriggerAnimation()
+    {
+        isOpen = !isOpen;
+
+        if(isOpen)
+        {
+            handle.hoverInfo = new V2_HandleHoverInfo("Close", null);
+            animator.SetTrigger("TriggerOpen");
+        } 
+        else
+        {
+            handle.hoverInfo = new V2_HandleHoverInfo("Open", null);
+            animator.SetTrigger("TriggerClose");
+        }
+
+    }
+}
