c64ecb1428aebf7a7cc6de7f8c7f09bed39db06d
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-09-21 17:43:51 +1200

    DocumentEditor layout WIP wireframe
    + LayoutImageAspect script
    + ParagraphInputField script
    + DocumentEditor updated (MainCanvas in ElijahSceneAlpha)

---
+++ Assets/BisonAlphaProduction/Scripts/DocumentEditor/V3_ParagraphInputField.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class V3_ParagraphInputField : MonoBehaviour
+{
+	private InputField inputField;
+	private LayoutElement layoutElement;
+
+	[SerializeField] private float extra = 60;
+
+	private void Awake()
+	{
+		inputField = GetComponent<InputField>();
+		layoutElement = GetComponent<LayoutElement>();
+		inputField.onValueChanged.AddListener(OnValueChanged);
+
+		OnValueChanged(inputField.text);
+	}
+
+	private bool inevent = false;
+
+	private void OnValueChanged(string text)
+	{
+		if (inevent) return;
+		inevent = true;
+		try
+		{
+			layoutElement.preferredHeight = inputField.preferredHeight + extra;
+		}
+		finally
+		{
+			inevent = false;
+		}
+	}
+}
