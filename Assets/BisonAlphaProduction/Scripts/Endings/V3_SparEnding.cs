453cb9019068c8c6478d54f043dacad6ad5dd007
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-09-24 15:44:20 +1200

    Ending video

---
+++ Assets/BisonAlphaProduction/Scripts/Endings/V3_SparEnding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class V3_SparEnding : MonoBehaviour
+{
+	[SerializeField]
+	private VideoClip m_videoClip;
+	public VideoClip videoClip { get => m_videoClip; private set => m_videoClip = value; }
+
+	[SerializeField]
+	private string m_nextSceneName;
+	public string nextSceneName { get => m_nextSceneName; private set => m_nextSceneName = value; }
+
+	public void Copy(V3_SparEnding other)
+	{
+		this.videoClip = other.videoClip;
+		this.nextSceneName = other.nextSceneName;
+	}
+}
