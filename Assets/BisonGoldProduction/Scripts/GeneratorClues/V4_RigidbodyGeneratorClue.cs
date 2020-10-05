c9daa6c460ee85ef2867baea4321612f0bcb67cf
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-10-05 13:55:55 +1300

    Randomly generated Fuse combinations.

---
+++ Assets/BisonGoldProduction/Scripts/GeneratorClues/V4_RigidbodyGeneratorClue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class V4_RigidbodyGeneratorClue : MonoBehaviour
+{
+	public V4_GeneratorClueRow[] rowsA = new V4_GeneratorClueRow[0];
+	public V4_GeneratorClueRow[] rowsB = new V4_GeneratorClueRow[0];
+
+	private void Awake()
+	{
+		InitThem("Engine A", rowsA);
+		InitThem("Engine B", rowsB);
+	}
+
+	private void InitThem(string theName, IEnumerable<V4_GeneratorClueRow> rows)
+	{
+		var engine = FindObjectsOfType<V3_Generator>().Where(c => c.name == theName).FirstOrDefault();
+		if (!engine)
+		{
+			Debug.LogError("Could not find Engine with name " + theName, this);
+		}
+		else
+		{
+			foreach (var row in rows)
+			{
+				row.generator = engine;
+			}
+		}
+	}
+}
---
+++ Assets/BisonGoldProduction/Scripts/GeneratorClues/V4_SparDb_Generator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+///		<para>See also:</para>
+///		<para><see cref="V3_GenPuzzleSolved"/></para>
+/// </summary>
+/// <changelog>
+///		<log author="Elijah Shadbolt" date="05/10/2020">
+///			<para>Created this script.</para>
+///		</log>
+/// </changelog>
+/// 
+public class V4_SparDb_GeneratorManager : V3_SparRandomizerDatabase<V4_SparData_GeneratorManager>
+{
+}
+
+/// <changelog>
+///		<log author="Elijah Shadbolt" date="05/10/2020">
+///			<para>Created this class.</para>
+///		</log>
+/// </changelog>
+/// 
+public class V4_SparData_GeneratorManager
+{
+	/// <summary>
+	/// Index correlates to index into <see cref="V3_GenPuzzleSolved.generators"/>.
+	/// </summary>
+	public IReadOnlyList<V4_SparData_Generator> map { get; private set; }
+
+	public V4_SparData_GeneratorManager(IReadOnlyList<V4_SparData_Generator> map)
+	{
+		this.map = map;
+	}
+}
+
+/// <changelog>
+///		<log author="Elijah Shadbolt" date="05/10/2020">
+///			<para>Created this class.</para>
+///		</log>
+/// </changelog>
+/// 
+public class V4_SparData_Generator
+{
+	public byte combination { get; private set; }
+
+	public V4_SparData_Generator(byte combination)
+	{
+		this.combination = combination;
+	}
+}
